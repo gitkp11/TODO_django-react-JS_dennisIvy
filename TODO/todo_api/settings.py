166a7dc46ccc633a4d0fefed736ba288ac811123
gitkp <47073116+gitkp11@users.noreply.github.com>
2021-01-31 19:14:34 +0530

    Update settings.py

--- TODO/todo_api/settings.py
+++ TODO/todo_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p$8gct177_o4mb#tee5qd-(j^9pqkbntu^kp4wvhik=+rp!cj#'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
